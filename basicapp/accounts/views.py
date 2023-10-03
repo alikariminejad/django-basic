49a4c4a15c2bed940f46d6a7f85875cc319dedb5
alikariminejad <kariminejad.ali.ak@gmail.com>
2023-10-03 10:14:02 +0330

    Register

--- basicapp/accounts/views.py
+++ basicapp/accounts/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserRegisterationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 
 def user_register(request):
-    return HttpResponse('User registeration page')
+    if request.method == 'POST':
+        form = UserRegisterationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(cd['username'],cd['email'],cd['password'])
+            user.first_name = cd['first_name']
+            user.last_name = cd['last_name']
+            user.save()
+            messages.success(request, 'user registered successfully','success')
+            return redirect('home')
+    else:
+        form = UserRegisterationForm()
+    return render(request, 'register.html',{'form':form})
