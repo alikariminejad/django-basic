6ae222b2da7addea6017c87864a681a33f1bc8ea
alikariminejad <kariminejad.ali.ak@gmail.com>
2023-10-03 11:26:22 +0330

    Login

--- basicapp/accounts/views.py
+++ basicapp/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterationForm
+from .forms import UserRegisterationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 
@@ -19,3 +20,22 @@ def user_register(request):
     else:
         form = UserRegisterationForm()
     return render(request, 'register.html',{'form':form})
+
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'],password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'logged in successfully','success')
+                return redirect('home')
+            else:
+                messages.error(request, 'username or password is wrong, try again.','danger')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html',{'form':form})
